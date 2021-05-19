dd5bd209be8d66c77fec4ae2c042321aa4c33479
raulmejia <r8a8m8p8@gmail.com>
2021-05-19 11:13:27 +0200

    at least 2 samples above minimum 1.0

--- Quality_Control_at_least_2_samples_above_a-minimum.R
+++ Quality_Control_at_least_2_samples_above_a-minimum.R
@@ -1,6 +1,5 @@
-# This script do the Quality control for Ncounter data
-# It calculates 2 standard deviations of the probe POS_E (That is supposed to be 0.5fM https://www.nanostring.com/wp-content/uploads/2020/12/Gene_Expression_Data_Analysis_Guidelines.pdf)
-# and adds that quantitiy to the mean of the Negative probes, 2*sd(POS_E)+mean(Negative_Controls) = the cut off
+# This script do Quality control of a matrix
+# It keeps only the rows that have more than 1 sample with values higher than a minimum (1 by default) 
 # The structure of your matrix should be 
 #          sample1 sample2 ...
 # rowname1  0.42    45
@@ -8,11 +7,8 @@
 # rowname3  45      0
 # NEG_Prob1 0       12
 # POS_E     2       1
-
 ## Notes
-## Make it flexible in order to change the name of the probe (POS_E) or other and the number of SD 
-##
-## Should I substact the treshould value to the final matrix or not?
+## Make it flexible in order to change the thresholds (number of samples) and minimum value 
 ############################## 
 ## Required libraries
 ##############################
@@ -56,19 +52,14 @@ args <- parser$parse_args()
 #############################
 ## The program starts
 #############################
-your_feature <- "POS_E"
+minimum_value <- 1
 inputdf <-read.table( file=args$inputfile, stringsAsFactors = FALSE )
 # inputdf <-read.table(file="/media/rmejia/mountme88/Projects/Maja-covid/Data/Controls/Ncounter_Platform/Kidney/toy_for_treshold.txt", stringsAsFactors = FALSE)
 
 path2save <- args$outputfile
-#  path2save <- "/media/rmejia/mountme88/Projects/Maja-covid/Data/Controls/Ncounter_Platform/Kidney/toy_for_treshold.txt_fM5.5_treshold.tsv"
+#  path2save <- "/media/rmejia/mountme88/Projects/Maja-covid/Data/Controls/Ncounter_Platform/Kidney/toy_for_tresshold_at_least_2_above_1.txt"
 
-# getting the mean from the negative controls
-mean_each_NEG_probe <- apply(inputdf[grep("NEG_",rownames(inputdf)),], 1, mean)
-mean_across_NEG_probes <- mean(mean_each_NEG_probe)
-treshold <- mean_across_NEG_probes + 2*sd(inputdf[your_feature,])
-
-logical_matrix <- inputdf > treshold
+logical_matrix <- inputdf > minimum_value
 rows_with_more_than_2_samples_above_the_treshold <- apply(logical_matrix,1,sum) > 1
 
 prunned_matrix <- inputdf[ rows_with_more_than_2_samples_above_the_treshold , ]
@@ -83,5 +74,3 @@ write.table( prunned_matrix, file = path2save , row.names = TRUE, sep="\t", col.
 ###########################
 lost_rows <- setdiff( rownames(inputdf) , rownames(prunned_matrix) )
 write.table( lost_rows, file = paste0(path2save,"_lost_rows") , row.names = TRUE, sep="\t", col.names = TRUE )
-
-
